db41aec6033954008081649d667c7de34dad5659
LN27100 <heloceinlove@hotmail.fr>
2023-10-23 16:45:39 +0200

    css cards modi

--- search.js
+++ search.js
@@ -12,7 +12,7 @@ class personnage {
 }
 
 let personnage1 = new personnage("49", "Sexy Boy", "homme19-25-1.jpg", "homme", "Normandie", "23", "Femme", "Je suis un amoureux de la nature.");
-let personnage2 = new personnage("50", "Au pays des merveilles", "femme19-25-1.jpg", "femme", "Auvergne", "19", "Homme", "J'adore la musique et la danse.");
+let personnage2 = new personnage("50", "Pays des merveilles", "femme19-25-1.jpg", "femme", "Auvergne", "19", "Homme", "J'adore la musique et la danse.");
 let personnage3 = new personnage("51", "Juste moi", "homme19-25-2.jpg", "homme", "Somme", "25", "Homme", "J'aime les voyages et l'aventure.");
 let personnage4 = new personnage("52", "CandyCrush", "femme19-25-2.jpg", "femme", "Auvergne", "20", "Femme", "Je suis passionnée de cuisine.");
 let personnage5 = new personnage("53", "Dany", "homme19-25-3.jpg", "homme", "Occitanie", "24", "Les deux", "Je suis fan de cinéma.");
@@ -24,7 +24,7 @@ let personnage10 = new personnage("58", "Shoot Boy", "homme26-35-2.jpg", "homme"
 let personnage11 = new personnage("59", "Geekeuse", "femme26-35-3.jpg", "femme", "Bourgogne", "29", "Femme", "Je suis une geek.");
 let personnage12 = new personnage("60", "Chaton tout doux", "homme26-35-3.jpg", "Normandie", "Liam", "28", "Les deux", "J'adore la lecture.");
 let personnage13 = new personnage("61", "Bella", "femme36-50-1.jpg", "femme", "Pays-de-la-Loire", "37", "Homme", "Je suis une amatrice d'art.");
-let personnage14 = new personnage("62", "Dark Vador", "homme36-50-1.jpg", "homme", "Provence-Alpes-Côte d'Azur", "42", "Femme", "Je suis fan de science-fiction.");
+let personnage14 = new personnage("62", "Dark Vador", "homme36-50-1.jpg", "homme", "Provence-Alpes", "42", "Femme", "Je suis fan de science-fiction.");
 let personnage15 = new personnage("63", "Globe Trotteuse", "femme36-50-2.jpg", "femme", "Bourgogne", "45", "Homme", "Je suis une voyageuse passionné.");
 let personnage16 = new personnage("64", "Le danseur", "homme36-50-2.jpg", "homme", "Occitanie", "48", "Femme", "J'adore la danse.");
 let personnage17 = new personnage("65", "Bob la bricoleuse", "femme36-50-3.jpg", "femme", "Ile-de-France", "38", "Les deux", "J'adore le bricolage.");
@@ -34,7 +34,7 @@ let personnage20 = new personnage("68", "Bisounours", "hommeplus50-1.jpg", "homm
 let personnage21 = new personnage("69", "Chichi", "femmeplus50-2.jpg", "femme", "Bretagne", "59", "Femme", "Je suis fan de jeux vidéo.");
 let personnage22 = new personnage("70", "Globe Trotteur", "hommeplus50-2.jpg", "homme", "Hauts-de-France", "61", "Homme", "J'adore les voyages.");
 let personnage23 = new personnage("71", "Douceur", "femmeplus50-3.jpg", "femme", "Centre-Val de Loire", "55", "Homme", "J'adore la danse de salon'.");
-let personnage24 = new personnage("72", "Grand matou", "hommeplus50-3.jpg", "homme", "Provence-Alpes Côte d'Azur", "64", "Femme", "J'adore les animaux.");
+let personnage24 = new personnage("72", "Grand matou", "hommeplus50-3.jpg", "homme", "Provence-Alpes", "64", "Femme", "J'adore les animaux.");
 
 
 const personnages = [
