36b926a4a66bc972d74865c253a0425a596ceae4
LN27100 <heloceinlove@hotmail.fr>
2023-10-23 12:54:34 +0200

    ajout photos et pseudos

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 class personnage {
-    constructor(id, pseudo, picture, gender, name, age, search, description) {
+    constructor(id, pseudo, picture, gender, region, age, search, description) {
         this.id = id;
         this.pseudo = pseudo;
         this.picture = picture;
         this.gender = gender;
-        this.name = name;
+        this.region = region;
         this.age = age;
         this.search = search;
         this.description = description;
     }
 }
 
-let personnage1 = new personnage(49, "", "image1.jpg", "homme", "John", 23, "Femme", "Je suis un amoureux de la nature.");
-let personnage2 = new personnage(50, "", "image2.jpg", "femme", "Alice", 19, "Homme", "J'adore la musique et la danse.");
-let personnage3 = new personnage(51, "", "image3.jpg", "homme", "Michael", 25, "Homme", "J'aime les voyages et l'aventure.");
-let personnage4 = new personnage(52, "", "image4.jpg", "femme", "Emily", 20, "Femme", "Je suis passionnée de cuisine.");
-let personnage5 = new personnage(53, "", "image5.jpg", "homme", "Daniel", 24, "Les deux", "Je suis fan de cinéma.");
-let personnage6 = new personnage(54, "", "image6.jpg", "femme", "Olivia", 21, "Homme", "J'adore les animaux.");
-let personnage7 = new personnage(55, "", "image7.jpg", "femme", "Sophie", 32, "Homme", "Je suis musicien.");
-let personnage8 = new personnage(56, "", "image8.jpg", "homme", "William", 26, "Femme", "J'adore les randonnées.");
-let personnage9 = new personnage(57, "", "image9.jpg", "femme", "Chloe", 33, "Homme", "Je suis un passionné de sport.");
-let personnage10 = new personnage(58, "", "image10.jpg", "homme", "James", 30, "Femme", "J'adore la photographie.");
-let personnage11 = new personnage(59, "", "image11.jpg", "femme", "Ella", 29, "Femme", "Je suis un geek.");
-let personnage12 = new personnage(60, "", "image12.jpg", "homme", "Liam", 28, "Les deux", "J'adore la lecture.");
-let personnage13 = new personnage(61, "", "image13.jpg", "femme", "Ava", 37, "Homme", "Je suis un amateur d'art.");
-let personnage14 = new personnage(62, "", "image14.jpg", "homme", "Henry", 42, "Femme", "Je suis fan de science-fiction.");
-let personnage15 = new personnage(63, "", "image15.jpg", "femme", "Mia", 45, "Homme", "Je suis un voyageur passionné.");
-let personnage16 = new personnage(64, "", "image16.jpg", "homme", "David", 48, "Femme", "J'adore la danse.");
-let personnage17 = new personnage(65, "", "image17.jpg", "femme", "Emma", 38, "Les deux", "Je suis un gourmand.");
-let personnage18 = new personnage(66, "", "image18.jpg", "homme", "Oliver", 40, "Femme", "J'adore le bricolage.");
-let personnage19 = new personnage(67, "", "image19.jpg", "femme", "Sophia", 54, "Homme", "Je suis un amateur de sports extrêmes.");
-let personnage20 = new personnage(68, "", "image20.jpg", "homme", "Noah", 52, "Femme", "Je suis une amoureuse de la nature.");
-let personnage21 = new personnage(69, "", "image21.jpg", "femme", "Isabella", 59, "Femme", "Je suis fan de jeux vidéo.");
-let personnage22 = new personnage(70, "", "image22.jpg", "homme", "Alexander", 61, "Homme", "J'adore les voyages.");
-let personnage23 = new personnage(71, "", "image23.jpg", "femme", "Grace", 55, "Homme", "Je suis musicienne.");
-let personnage24 = new personnage(72, "", "image24.jpg", "homme", "Ethan", 64, "Femme", "J'adore les animaux.");
+let personnage1 = new personnage(49, "Sexy Boy", "img/homme19-25-1.jpg", "homme", "Normandie", 23, "Femme", "Je suis un amoureux de la nature.");
+let personnage2 = new personnage(50, "Au pays des merveilles", "img/femme19-25-1.jpg", "femme", "Auvergne", 19, "Homme", "J'adore la musique et la danse.");
+let personnage3 = new personnage(51, "Juste moi", "img/homme19-25-2.jpg", "homme", "Somme", 25, "Homme", "J'aime les voyages et l'aventure.");
+let personnage4 = new personnage(52, "CandyCrush", "img/femme19-25-2.jpg", "femme", "Auvergne", 20, "Femme", "Je suis passionnée de cuisine.");
+let personnage5 = new personnage(53, "Dany", "img/homme19-25-3.jpg", "homme", "Occitanie", 24, "Les deux", "Je suis fan de cinéma.");
+let personnage6 = new personnage(54, "Queeny", "img/femme19-25-3.jpg", "femme", "Normandie", 21, "Homme", "J'adore les animaux.");
+let personnage7 = new personnage(55, "La violoniste", "img/femme26-35-1.jpg", "femme", "Bretagne", 32, "Homme", "Je suis musicienne.");
+let personnage8 = new personnage(56, "Will91", "img/homme26-35-1.jpg", "homme", "Ile-de-France", 26, "Femme", "J'adore les randonnées.");
+let personnage9 = new personnage(57, "Bella", "img/femme26-35-2.jpg", "femme", "Bretagne", 33, "Homme", "Je suis une passionnée de sport.");
+let personnage10 = new personnage(58, "Shoot Boy", "img/homme26-35-2.jpg", "homme", "Hauts-de-France", 30, "Femme", "J'adore la photographie.");
+let personnage11 = new personnage(59, "Geekeuse", "img/femme26-35-3.jpg", "femme", "Bourgogne", 29, "Femme", "Je suis une geek.");
+let personnage12 = new personnage(60, "Chaton tout doux", "img/homme26-35-3.jpg", "Normandie", "Liam", 28, "Les deux", "J'adore la lecture.");
+let personnage13 = new personnage(61, "Bella", "img/femme36-50-1.jpg", "femme", "Pays-de-la-Loire", 37, "Homme", "Je suis une amatrice d'art.");
+let personnage14 = new personnage(62, "Dark Vador", "img/homme36-50-1.jpg", "homme", "Provence-Alpes-Côte d'Azur", 42, "Femme", "Je suis fan de science-fiction.");
+let personnage15 = new personnage(63, "Globe Trotteuse", "img/femme36-50-2.jpg", "femme", "Bourgogne", 45, "Homme", "Je suis une voyageuse passionné.");
+let personnage16 = new personnage(64, "Le danseur", "img/homme36-50-2.jpg", "homme", "Occitanie", 48, "Femme", "J'adore la danse.");
+let personnage17 = new personnage(65, "Bob la bricoleuse", "img/femme36-50-3.jpg", "femme", "Ile-de-France", 38, "Les deux", "J'adore le bricolage.");
+let personnage18 = new personnage(66, "Sweety Candy", "img/homme36-50-3.jpg", "homme", "Nouvelle-Aquitaine", 40, "Femme", "Je suis un gourmand.");
+let personnage19 = new personnage(67, "Sophiadu80", "img/femmeplus50-1.jpg", "femme", "Grand-Est", 54, "Homme", "Je suis une amatrice de sports extrêmes.");
+let personnage20 = new personnage(68, "Bisounours", "img/hommeplus50-1.jpg", "homme", "Ile-de-France", 52, "Femme", "Je suis une amoureuse de la nature.");
+let personnage21 = new personnage(69, "Chichi", "img/femmeplus50-2.jpg", "femme", "Bretagne", 59, "Femme", "Je suis fan de jeux vidéo.");
+let personnage22 = new personnage(70, "Globe Trotteur", "img/hommeplus50-2.jpg", "homme", "Hauts-de-France", 61, "Homme", "J'adore les voyages.");
+let personnage23 = new personnage(71, "Douceur", "img/femmeplus50-3.jpg", "femme", "Centre-Val de Loire", 55, "Homme", "J'adore la danse de salon'.");
+let personnage24 = new personnage(72, "Grand matou", "img/hommeplus50-3.jpg", "homme", "Provence-Alpes Côte d'Azur", 64, "Femme", "J'adore les animaux.");
 
 
 const personnages = [
