35864f3ac8179a706e5ec0a9def96f03088fe67e
Omar <omarsegura76300@gmail.com>
2023-10-25 11:19:28 +0200

    modif button

--- JS/search.js
+++ JS/search.js
@@ -112,7 +112,7 @@ function affichage() {
                          <li>Région : ${hommes[i].region}</li>
                          </ul>
                          <p>${hommes[i].description}</p>
-                         <button href="../HTML/voirPlus.html">Voir plus</button>
+                         <a href="../HTML/voirPlus.html><button>Voir plus</button><a>
                         `
 
             infos.appendChild(card)
@@ -133,7 +133,7 @@ function affichage() {
                          <li>Région : ${femmes[i].region}</li>
                          </ul>
                          <p>${femmes[i].description}</p>
-                         <button href="../HTML/voirPlus.html">Voir plus</button>
+                         <a href="../HTML/voirPlus.html><button>Voir plus</button><a>
                         `
             infos.appendChild(card)
         }
@@ -155,7 +155,7 @@ function affichage() {
                          <li>Région : ${All[i].region}</li>
                          </ul>
                          <p>${All[i].description}</p>
-                         <a><button>Voir plus</button></a>
+                         <a href="../HTML/voirPlus.html><button>Voir plus</button><a>
                         `
 
             infos.appendChild(card)
